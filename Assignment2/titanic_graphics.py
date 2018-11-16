461536739dea4da759de5dad1d5fdbec2de4c6b7
lberger1993 <berger.luciaeve@gmail.com>
2018-11-16 12:02:14 +0100

    Adds 3rd graphs

--- Assignment2/titanic_graphics.py
+++ Assignment2/titanic_graphics.py
@@ -31,7 +31,7 @@ def create_bar_chart():
 def create_class_sp():
     survival_rate = df.loc[df['survived'] == 1]
     death_rate = df.loc[df['survived'] == 0]
-    rates_of_survival = {'1':0, '2':0, '3':0}
+    rates_of_survival = {'1': 0, '2': 0, '3': 0}
     rates_of_death = {'1': 0, '2': 0, '3': 0}
     for i in range(1,4):
         rates_of_survival[str(i)] = len(survival_rate.loc[survival_rate['pclass'] == i])/len(survival_rate)
@@ -59,6 +59,59 @@ def create_wedge_chart(data, colors, title):
     return p
 
 
+from bokeh.core.properties import value
+from bokeh.models import ColumnDataSource
+
+def survival_rate_by_age():
+    age_sorted = df.sort_values(by='age')
+    columns = ['age_bracket', 'count', 'survival', 'male', 'female']
+    lst = list()
+    for i in range(1, 90, 10):
+        subset = age_sorted.loc[(age_sorted["age"] < i + 10) & (age_sorted["age"] > i - 10)]
+        count_of_male = len(subset.loc[
+            (subset['sex'] == 'male') & (subset['survived'] == 1)])
+        count_of_female = len(subset.loc[
+                                (subset['sex'] == 'female') & (subset['survived'] == 1)])
+
+        lst.append([i, len(subset), len(subset.loc[subset['survived'] == 1]) / len(subset),
+                    count_of_male, count_of_female])
+
+    from bokeh.transform import dodge
+    age_survival_dict = pd.DataFrame(lst, columns=columns)
+    line_chart = figure(plot_width=400, plot_height=400)
+    line_chart.line(source=age_survival_dict, x='age_bracket', y='survival', line_width=2)
+
+
+    age_cohorts = ['0-10', '11-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70+']
+    sex = ["male", "female"]
+    colors = ["#c9d9d3", "#718dbf"]
+
+    data = {'age_cohorts': age_cohorts,
+            'male': age_survival_dict['male'][:8],
+            'female': age_survival_dict['female'][:8]}
+    source = ColumnDataSource(data=data)
+
+    p = figure(x_range=age_cohorts, y_range=(0, 300), plot_height=250, title="Fruit Counts by Year",
+               toolbar_location=None, tools="")
+
+    p.vbar(x=dodge('age_cohorts', -0.25, range=p.x_range), top='male', width=0.2, source=source,
+           color="#c9d9d3", legend=value("male"))
+
+    p.vbar(x=dodge('age_cohorts', 0.0, range=p.x_range), top='female', width=0.2, source=source,
+           color="#718dbf", legend=value("female"))
+
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xgrid.grid_line_color = None
+    p.axis.minor_tick_line_color = None
+    p.outline_line_color = None
+    p.legend.location = "top_left"
+    p.legend.orientation = "horizontal"
+    show(row(line_chart,p))
+
+
 filename = "titanic3.csv"
 df = pd.read_csv( filename, header=0 )
-create_class_sp()
+#create_class_sp()
+survival_rate_by_age()
+
