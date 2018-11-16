b9cea7d91a37fd92c46433f03b7965d4bca84b9d
Kumail Raza <sraza@rhrk.uni-kl.de>
2018-11-16 22:18:16 +0100

    Assignment 1 Final

--- Assignment1/exercise3.py
+++ Assignment1/exercise3.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import row
 from bokeh.models import NumeralTickFormatter, Span, BoxAnnotation
+from bokeh.models import LinearColorMapper, LogTicker, ColorBar
+
 
 df = pd.read_csv("genre_data.csv")
 # print(df['Date'])
@@ -23,8 +25,9 @@ p = figure(title="Gross of Film by Year in Genre Biopic -> Music",
            x_axis_label='year',
            y_axis_label='total gross',
            x_range=(1979,2019),
-           tools="hover", tooltips="Straight out of Compton (2015) ")
+           tools="hover", tooltips="Straight out of Compton (2015): " + "$y{int}" + " $ million")
 
+label = ['$0.00', '$200.00 m']
 
 palette = ["#053061", "#2166ac", "#4393c3", "#92c5de", "#d1e5f0",
            "#f7f7f7", "#fddbc7", "#f4a582", "#d6604d", "#b2182b",
@@ -38,12 +41,20 @@ df['melting_colors'] = [palette[i] for i in colors_inds]
 
 
 #p.line(y=y, x=x_axis, legend="Weekly Gross (1989-1990)", line_width=2)
-p.circle(x=x_axis, y=df['Gross'], fill_alpha=0.9, size=20, color=df['melting_colors'])
+p.circle(x=x_axis, y=df['Gross'], fill_alpha=0.9, size=20, color=df['melting_colors'], legend='Weekly Gross in Million $')
+p.legend.location = "top_left"
+
 low_box = BoxAnnotation(top=50000000, fill_alpha=0.1, fill_color='red')
 
 # use a formatter to display y-axis tick labels in million dollars
 p.yaxis[0].formatter = NumeralTickFormatter(format="($ 0.00 a)")
 p.add_layout(low_box)
+
+# add a colour map for y-axis
+color_mapper = LinearColorMapper(palette=palette, low=0, high=200)
+color_bar = ColorBar(color_mapper=color_mapper, ticker=LogTicker(),label_standoff=12, border_line_color=None, location=(0,0))
+p.add_layout(color_bar, 'right')
+
 show(row(p))
 
 
